396b652c602d668548430a06a089c360a75b7bb3
voloshmiak <voloshenkomykhailo@gmail.com>
2025-08-01 22:15:12 +0300

    renamed package, added currency type

--- payment-service/data/transaction.go
+++ payment-service/data/transaction.go
@@ -18,11 +18,49 @@ const (
 	PayPal     PaymentMethod = "paypal"
 )
 
+type Currency string
+
+const (
+	EUR Currency = "EUR"
+	USD Currency = "USD"
+	JPY Currency = "JPY"
+	BGN Currency = "BGN"
+	CZK Currency = "CZK"
+	DKK Currency = "DKK"
+	GBP Currency = "GBP"
+	HUF Currency = "HUF"
+	PLN Currency = "PLN"
+	RON Currency = "RON"
+	SEK Currency = "SEK"
+	CHF Currency = "CHF"
+	ISK Currency = "ISK"
+	NOK Currency = "NOK"
+	HRK Currency = "HRK"
+	RUB Currency = "RUB"
+	TRY Currency = "TRY"
+	AUD Currency = "AUD"
+	BRL Currency = "BRL"
+	CAD Currency = "CAD"
+	CNY Currency = "CNY"
+	HKD Currency = "HKD"
+	IDR Currency = "IDR"
+	ILS Currency = "ILS"
+	INR Currency = "INR"
+	KRW Currency = "KRW"
+	MXN Currency = "MXN"
+	MYR Currency = "MYR"
+	NZD Currency = "NZD"
+	PHP Currency = "PHP"
+	SGD Currency = "SGD"
+	THB Currency = "THB"
+	ZAR Currency = "ZAR"
+)
+
 type Transaction struct {
 	ID                   int64         `json:"id"`
 	OrderID              int64         `json:"order_id"`
 	Amount               float64       `json:"amount"`
-	Currency             string        `json:"currency"`
+	Currency             Currency      `json:"currency"`
 	Status               Status        `json:"Status"`
 	GatewayTransactionID string        `json:"gateway_transaction_id"`
 	PaymentMethod        PaymentMethod `json:"payment_method"`
@@ -34,7 +72,7 @@ var Transactions = []*Transaction{
 		ID:                   1,
 		OrderID:              101,
 		Amount:               1000,
-		Currency:             "USD",
+		Currency:             USD,
 		Status:               Completed,
 		GatewayTransactionID: "txn_123456789",
 		PaymentMethod:        CreditCard,
@@ -44,7 +82,7 @@ var Transactions = []*Transaction{
 		ID:                   2,
 		OrderID:              102,
 		Amount:               2000,
-		Currency:             "USD",
+		Currency:             USD,
 		Status:               Pending,
 		GatewayTransactionID: "txn_987654321",
 		PaymentMethod:        PayPal,
@@ -66,7 +104,7 @@ func AddTransaction(orderID int64, amount float64, currency, paymentMethod strin
 		ID:                   int64(len(Transactions) + 1),
 		OrderID:              orderID,
 		Amount:               amount,
-		Currency:             currency,
+		Currency:             Currency(currency),
 		Status:               Pending,
 		GatewayTransactionID: "txn_" + time.Now().Format("20060102150405"),
 		PaymentMethod:        PaymentMethod(paymentMethod),
